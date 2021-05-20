c5786b6ae5d94494fecc28e52165d2194aae8be6
choidoorim <doorim321@naver.com>
2021-05-20 17:40:39 +0900

    revise gitignore

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 // TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
 // TODO: .gitignore에 추가하는거 앚지 마세요!
 module.exports = {
-    'jwtsecret' :  'softsquared_Alvin_jwt_secret_key_09010806',
+    'jwtsecret' :  'softsquared_Alvin_jwt_secret_key_09010806'
 };
